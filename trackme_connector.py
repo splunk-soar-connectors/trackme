e2b0850e929a4763e2c56bfcd70c0b6972ebde1c
Jacob Davis <jacobd@splunk.com>
2026-07-17 10:15:42 -0700

    chore: normalize TrackMe package metadata
    PSAAS-31373 (VULN-94098, FS-2252).
    
    Retain the exact dev-cicd-tools v2.2.4 package-generated metadata and documentation output. No application dependencies are declared, so no transitive dependency update was generated.
    
    Written by Codex.

--- trackme_connector.py
+++ trackme_connector.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # File: trackme_connector.py
 #
-# Copyright (c) TrackMe Limited, 2024-2025
+# Copyright (c) TrackMe Limited, 2024-2026
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
